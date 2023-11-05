af76da0dbdc81e156f90f067573af4c452d5bd10
Rain Chen <87171700+CongJyu@users.noreply.github.com>
2023-11-05 13:13:04 +0800

    add lab1 dsp-lab-03

---
+++ lab_1/dsp-lab-03.py
@@ -0,0 +1,25 @@
+# encoding utf-8
+# python 3.12
+
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import signal
+
+t1 = np.linspace(-1, 1, 10000, endpoint=False)
+wave1 = signal.square(2 * np.pi * 10 * t1, 1 / 3) + 1
+plt.subplot(2, 1, 1)
+plt.plot(t1, wave1)
+plt.title('Original')
+plt.xlim(-0.1, 0.1)
+plt.ylim(-1, 3)
+
+t2 = np.linspace(-1, 1, 16 * 2 * 10, endpoint=False)
+wave2 = signal.square(2 * np.pi * 10 * t2, 1 / 3) + 1
+
+plt.subplot(2, 1, 2)
+plt.stem(t2, wave2)
+plt.title('Sample')
+plt.xlim(-0.1, 0.1)
+plt.ylim(-1, 3)
+
+plt.show()
