f26a72e8b12556d2a7ad28c48c38b62c2f5897a0
Rain Chen <87171700+CongJyu@users.noreply.github.com>
2023-11-05 12:33:38 +0800

    add dsp-lab1 problem 2-a

---
+++ lab_1/dsp-lab-02-a.py
@@ -0,0 +1,34 @@
+# encoding utf-8
+# python 3.12
+
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy as spy
+
+np.random.seed(2023)
+
+n = np.arange(-10, 11, 1)
+x1 = np.random.uniform(-1, 1, 21)
+x2 = np.random.uniform(-1, 1, 21)
+
+plt.subplot(3, 1, 1)
+plt.stem(n, x1)
+plt.title('$ x1 $')
+plt.xlabel('$ n $')
+plt.ylabel('$ x1 $')
+
+plt.subplot(3, 1, 2)
+plt.stem(n, x2)
+plt.title('$ x2 $')
+plt.xlabel('$ n $')
+plt.ylabel('$ x2 $')
+
+x3 = np.roll(x1, 6)
+x3[0:6] = 0
+plt.subplot(3, 1, 3)
+plt.stem(n, x3)
+plt.title('$ x3 $')
+plt.xlabel('$ n $')
+plt.ylabel('$ x3 $')
+
+plt.show()
