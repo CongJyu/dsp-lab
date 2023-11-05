e9452d0cdd48ee1a063b679be79cd01052bbf0aa
Rain Chen <87171700+CongJyu@users.noreply.github.com>
2023-11-05 12:46:05 +0800

    add lab1 dsp-lab-02-c

---
+++ lab_1/dsp-lab-02-c.py
@@ -0,0 +1,36 @@
+# encoding utf-8
+# python 3.12
+
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy as spy
+
+np.random.seed(2023)
+
+n = np.arange(-10, 11, 1)
+x1 = np.random.uniform(-1, 1, 21)
+x2 = np.random.uniform(-1, 1, 21)
+
+plt.subplot(3, 1, 1)
+plt.stem(n, x1)
+plt.title('$ x1 $')
+plt.xlabel('$ n $')
+plt.ylabel('$ x1 $')
+
+plt.subplot(3, 1, 2)
+plt.stem(n, x2)
+plt.title('$ x2 $')
+plt.xlabel('$ n $')
+plt.ylabel('$ x2 $')
+
+x_rolled = np.roll(x2, 1)
+x_rolled[0:1] = 0
+w = x1 * x_rolled
+
+plt.subplot(3, 1, 3)
+plt.stem(n, w)
+plt.title('$ w $')
+plt.xlabel('$ n $')
+plt.ylabel('$ w $')
+
+plt.show()
