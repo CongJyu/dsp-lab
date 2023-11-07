2d88bcfed3352364519c5d59c146a62c03ebf7df
Rain Chen <87171700+CongJyu@users.noreply.github.com>
2023-11-07 10:29:32 +0800

    correct lab1 04 a

--- lab_1/dsp-lab-04-a.py
+++ lab_1/dsp-lab-04-a.py
@@ -6,28 +6,15 @@ import matplotlib.pyplot as plt
 from scipy import signal
 
 
-def x_a(t):
-    return t * np.exp(-t) * (t >= 0)
+omega = np.arange(-10, 10, 0.001)
 
 
-def X_a(w):
-    return 1 / ((1 + 1j * w) ** 2)
+def X_a(omega):
+    return 1 / (1 + 1j * omega) ** 2
 
 
-system = signal.lti([], X_a)
-t, y = signal.impulse(signal, N=1000)  # the sentence is strange
-
-plt.figure(figsize=(10, 5))
-plt.subplot(1, 2, 1)
-t1 = np.linspace(0, 10, 1000)
-plt.plot(t1, x_a(t1), label='$ x_a(t) $')
-plt.xlabel('$ t $')
-plt.ylabel('$ x_a(t) $')
-plt.legend()
-
-plt.subplot(t, y, label='Impulse Response')
-plt.xlabel('$ t $')
-plt.ylabel('$ y(t) $')
-plt.legend()
+plt.plot(omega, np.abs(X_a(omega)))
+plt.xlabel('$ \Omega $')
+plt.ylabel('$ X_a(j \Omega) $')
 
 plt.show()
