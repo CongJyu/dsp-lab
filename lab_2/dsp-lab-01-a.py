f79ab5fc9da59769857e452aa49e8a57a13e1af9
Rain Chen <87171700+CongJyu@users.noreply.github.com>
2023-11-27 22:20:05 +0800

    add dsp-lab2-01-a

---
+++ lab_2/dsp-lab-01-a.py
@@ -0,0 +1,18 @@
+# encoding utf-8
+# python 3.10
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# 定义周期序列
+x = np.array([0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3])
+
+# 计算离散傅立叶级数
+X = np.fft.fft(x)
+
+# 绘制幅度特性
+plt.stem(np.abs(X))
+plt.title('DFS Amplitude Characteristic')
+plt.xlabel('k')
+plt.ylabel('|X(k)|')
+plt.show()
