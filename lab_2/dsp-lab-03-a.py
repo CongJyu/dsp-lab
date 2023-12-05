2e7b68dd5abb657371207190e70c020079ec51ec
Rain Chen <87171700+CongJyu@users.noreply.github.com>
2023-12-06 00:30:34 +0800

    update lab2-03-a

--- lab_2/dsp-lab-03-a.py
+++ lab_2/dsp-lab-03-a.py
@@ -4,4 +4,19 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+x = np.array([
+    0, 1, 2, 3, 4, 5, 6, 7,
+    8, 9, 10, 11, 12, 13, 14, 15
+])
+h = np.array([1, 1, 1, 1, 1, 1, 1, 1])
+N = x.shape[-1]  # 序列长度
 
+# 圆周卷积
+result = np.fft.fft(np.fft.ifft(x, n=N) * np.fft.ifft(h, n=N)).real
+
+print(result)
+plt.stem(result)
+plt.xlabel('n')
+plt.ylabel('result')
+plt.grid(True)
+plt.show()
